00863ed9010680096b0ad5116de861c4aa43e279
YuehChuan <schwarmcyc@hotmail.com>
2023-04-20 16:52:28 +0800

    project skeleton

---
+++ src/test/test.cpp
@@ -0,0 +1,13 @@
+#include <boost/mpl/vector.hpp>
+#include <boost/preprocessor/seq/enum.hpp>
+#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/seq/transform.hpp>
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/comparison.hpp>
+#include <boost/utility.hpp>
+#include<iostream>
+int main()
+{
+std::cout<<"a"<<std::endl;
+
+}
